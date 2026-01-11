8649bf1162e4bfc014624e39bd16ed4fc5bc7b57
HrHossain <mofazzelhossain185@gmail.com>
2026-01-11 20:11:31 +0600

    add todo custom error class for handle error

--- js/error.js
+++ js/error.js
@@ -10,3 +10,15 @@ export class LoginErr extends Error{
     }
 }
 
+export class TodoErr extends Error{
+   
+    constructor(message, options = {}){
+        super(message)
+        this.code = options.code ?? "TODO_ERROR"
+        this.status = options.status ?? "401"
+    }
+    static customError(message , options){
+        throw new TodoErr(message , options)
+    }
+}
+
